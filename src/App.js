36130fb248da2aa60eddcc4dd09bd4e8ac9a3a79
Ali Fadda <silvertechguy@gmail.com>
2020-11-01 16:56:01 -0800

    finish the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Video from "./components/video/Video";
+import { db } from "./firebase";
 
 function App() {
-  return <div className="App">Hello, React!</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(
+          ({ messages, url, likes, shares, description, channel, song }, i) => {
+            return (
+              <Video
+                key={i}
+                messages={messages}
+                likes={likes}
+                shares={shares}
+                description={description}
+                channel={channel}
+                song={song}
+                url={url}
+              />
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
